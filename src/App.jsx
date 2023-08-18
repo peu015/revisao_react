c555404f1ed3ccb91c099557f28f35c5123f0d29
Pietro <pietropestana@gmail.com>
2023-08-18 15:43:41 -0300

    fim

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,23 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [email, setEmail] = useState("")
+  const [senha, setSenha] = useState("")
+
+  function handleEmail(evento){
+    setEmail(evento.target.value)
+  }
+
+  function handleSenha(evento){
+    setSenha(evento.target.value)
+  }
+
+  function handleSubmit(){
+    alert(`
+    seu e-mail é: ${email}
+    sua senha é: ${senha}
+    `)
+  }
 
   return (
     <>
@@ -10,15 +27,18 @@ function App() {
       </header>
 
       <main>
-        <form action="">
+        <form action="" onSubmit={handleSubmit}>
           <div>
             <label htmlFor="email">E-mail: </label>
-            <input id='email' type="email" placeholder='Digite seu e-mail'/>
+            <input id='email' type="email" placeholder='Digite seu e-mail' onChange={handleEmail}/>
           </div>
           <div>
             <label htmlFor="senha">Senha: </label>
-            <input id='senha' type="password" placeholder='Digite sua senha'/>
+            <input id='senha' type="password" placeholder='Digite sua senha' onChange={handleSenha}/>
           </div>
+
+          <button>Send</button>
+
         </form>
       </main>
     </>
